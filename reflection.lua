fa1ba537887fd42c05343efff354354c5a2382aa
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-07-15 17:42:32 +1200

    Initial commit

---
+++ reflection.lua
@@ -0,0 +1,20 @@
+local empty_path = "data/reflection/_empty.txt"
+ModTextFileSetContent(empty_path, "")
+local whoami = ModImageWhoSetContent(empty_path)
+local _require = require
+local _require_path = require_path
+
+---@param modname string
+---@return string
+function require_path(modname)
+	return ("mods/%s/lib/reflection/%s.lua"):format(whoami, (modname:gsub("%.", "/")))
+end
+
+---@param modname string
+---@return any
+function require(modname)
+	return dofile_once(require_path(modname))
+end
+
+require_path = _require_path
+require = _require
